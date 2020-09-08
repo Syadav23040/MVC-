a959a50d0aba9b3fa02bd6b76cc61f24bd5ad0c0
Syadav23040 <sumanyadav9849@gmail.com>
2020-09-08 12:16:03 +0530

    After branching

---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Emp
+    {
+        public int Eid{ get; set; }
+        public string Ename { get; set; }
+        public double Esalary { get; set; }
+        public string  Designation { get; set; }
+    }
+}
